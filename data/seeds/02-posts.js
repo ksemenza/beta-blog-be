7c7f30b9489c99f8760f8895517fdd394ad955a9
ksemenza <ksemenza13@gmail.com>
2020-12-10 13:35:33 -0500

    add comment post id

--- data/seeds/02-posts.js
+++ data/seeds/02-posts.js
@@ -6,18 +6,25 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('posts').insert([
         {
+             "author": "Kim",
+          "created_at": "2018-12-10 17:28:54",
           "title":"1 Title By Kim",
            "content":"This is where my blog goes",
            "topic":"computer science",
            "user_id":"1"
+           
        },
         {
+          "author": "Kim",
+          "created_at": "2018-10-10 17:28:54",
           "title":"New Title By Kim",
            "content":"This is where the details are",
            "topic":"design",
            "user_id":"1"
         },
         {
+          "author": "Colleen",
+          "created_at": "2020-12-10 17:15:54",
           "title":"Title By Colleen",
            "content":"My boss is Crazy",
            "topic":"education",
@@ -30,24 +37,32 @@ exports.seed = function(knex) {
            "user_id":"2"
        },
         {
+          "author": "Dana",
+          "created_at": "2019-06-10 15:28:54",
           "title":"New Title By Dana",
            "content":"I love my family",
            "topic":"Family",
            "user_id":"3"
        },
         {
+          "author": "Dana",
+          "created_at": "2019-02-10 15:28:54",
           "title":"Title By Dana",
            "content":"Importing never ends",
            "topic":"business",
            "user_id":"3"
        },
         {
+          "author": "Shanna",
+          "created_at": "2020-02-10 01:28:54",
           "title":"Title By Shanna",
            "content":"Passive Aggressive Defense ",
            "topic":"psychology",
            "user_id":"4"
        },
         {
+          "author": "Shanna",
+          "created_at": "2020-02-08 01:28:54",
           "title":"New Title By Shanna",
            "content":"I love gardening",
            "topic":"landscaping",
--- data/seeds/03-comments.js
+++ data/seeds/03-comments.js
@@ -5,19 +5,25 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('comments').insert([
-        {
+        { "author":"Kim",
           "comment":"Computers are Cool",
           "post_id":"1"
       },
         {
+          "author": "Colleen",
+          "created_at": "2018-12-10 17:28:54",
           "comment":"Teaching is hard",
           "post_id":"3"
       },
         {
+          "author": "Dana",
+          "created_at": "2020-11-10 17:28:54",
           "comment":"Kids are wonderful",
           "post_id":"5"
       },
         {
+           "author": "Shanna",
+          "created_at": "2020-12-01 17:28:54",
           "comment":"DBT techniques",
           "post_id":"7"
       },

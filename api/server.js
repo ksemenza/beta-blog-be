478eba7449db5a4e165c9ef9314a5d6aa95984d5
ksemenza <ksemenza13@gmail.com>
2020-12-30 16:54:21 -0500

    updating migration img

--- api/server.js
+++ api/server.js
@@ -23,11 +23,12 @@ server.use('/upload' , express.static(path.join(__dirname, './uploads')))
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads')
+        cb(null, './uploads')
     },
     filename: function (req, file, cb) {
         console.log(file)
-        cb(null, Date.now() + path.extname(file.originalname));
+        cb(null, file.originalname);
+        // cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 
@@ -40,15 +41,25 @@ const fileFilter = (req, file, cb) => {
 }
 
 const upload = multer({ storage: storage, fileFilter: fileFilter })
-server.post('/uploads', upload.single('image', (req, res, next) => {
+server.post('/api/uploads', upload.single('image', (req, res, next) => {
     try {
-        return res.status(201).json({
+         res.status(201).json({
             message: 'File uploaded successfully'
-        });
+         });
+        res.send(req.file, { message: 'File uploaded' })
     } catch (error) {
         console.error(error);
     }
 }))
+
+server.post('/api/uploads/bulk', upload.array('image', 4), (req, res) => {
+    try {
+        res.send(req.files)
+    } catch (error) {
+        console.log(error)
+        res.send(400)
+    }
+})
 
 const userRouter = require('../user/user-router.js')
 const postRouter = require('../routers/post-router.js')

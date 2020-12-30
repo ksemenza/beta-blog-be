291118966370247b80f3ff8a00daa840c9ea3a38
ksemenza <ksemenza13@gmail.com>
2020-12-30 15:22:38 -0500

    upload profile image

--- api/server.js
+++ api/server.js
@@ -4,11 +4,10 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const path = require('path')
 
-const userRouter = require('../user/user-router.js')
-const postRouter = require('../routers/post-router.js')
-const commentRouter = require('../routers/comment-router.js')
-const notificationRouter = require('../routers/notification-router.js')
+const multer = require('multer');
+
 
 const server = express();
 
@@ -20,10 +19,48 @@ server.use(helmet());
 server.use(morgan('tiny'));
 server.use(cors());
 
+server.use('/upload' , express.static(path.join(__dirname, './uploads')))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        console.log(file)
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter })
+server.post('/uploads', upload.single('image', (req, res, next) => {
+    try {
+        return res.status(201).json({
+            message: 'File uploaded successfully'
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}))
+
+const userRouter = require('../user/user-router.js')
+const postRouter = require('../routers/post-router.js')
+const commentRouter = require('../routers/comment-router.js')
+const notificationRouter = require('../routers/notification-router.js')
+
+
 server.use('/api/auth', userRouter)
 server.use('/api/post', postRouter)
 server.use('/api/comment', commentRouter)
 server.use('/api/notification', notificationRouter)
+
 
 
 const PORT =  process.env.PORT || 8080;

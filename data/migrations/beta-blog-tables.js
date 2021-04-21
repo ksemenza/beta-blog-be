24dee058c69e504bd2ba7c166a37e0ceb9b8f573
ksemenza <ksemenza13@gmail.com>
2021-04-21 16:34:58 -0400

    updated files

--- data/migrations/beta-blog-tables.js
+++ data/migrations/beta-blog-tables.js
@@ -1,62 +1,74 @@
+exports.up = function (knex, Promise) {
+  return knex.schema
 
-exports.up = function(knex, Promise) {
-    return   knex.schema.createTable('users', tbl => {
-        tbl.uuid('user_id').primary()
-        tbl.string('first_name', 255).notNullable();
-        tbl.string('last_name', 255).notNullable();
-        tbl.string('username', 255).notNullable().unique();
-          tbl.string('email', 255).notNullable().unique();
-          tbl.string('password', 255).notNullable()       
-          tbl.timestamp('created_at').defaultTo(knex.fn.now()) 
-
-          
-      })
-
-      .createTable('posts', tbl => {
-        tbl.integer('post_id').index()
-        tbl.timestamp('created_at').defaultTo(knex.fn.now()) 
-
-        tbl.integer('author').unsigned().notNullable();
-        tbl.string('title', 255).notNullable()
-        tbl.string('topic', 255)
-        tbl.string('content').notNullable()
-        tbl.foreign('author').references('user_id').inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE');
-
-        
+    .createTable("users", (tbl) => {
+      tbl.increments("id").index().unique();
+      tbl.string("first_name", 255).notNullable();
+      tbl.string("last_name", 255).notNullable();
+      tbl.string("username", 255).notNullable().unique();
+      tbl.string("email", 255).notNullable().unique();
+      tbl.string("password", 255).notNullable();
     })
 
-    .createTable('comments', tbl => {
-        tbl.string('commenter').notNullable()
-        tbl.timestamp('created_at').defaultTo(knex.fn.now()) 
-
-        tbl.string('comment').notNullable()
-        .references('post_id').inTable('post').onUpdate('CASCADE').onDelete('CASCADE')
-        tbl.foreign('commenter').references('username').inTable('users')
-
-        
-
+    .createTable("topics", (tbl) => {
+      tbl.increments("id").index().unique();
+      tbl.string("subject").notNullable();
     })
-    .createTable('notifications', tbl => {
-        tbl.string('user').notNullable()
-        tbl.timestamp('created_at').defaultTo(knex.fn.now()) 
 
-        tbl.string('reaction').notNullable()
-        .references('post_id').inTable('post').onUpdate('CASCADE').onDelete('CASCADE')
-        tbl.foreign('user').references('username').inTable('users')
-        
-
+    .createTable("posts", (tbl) => {
+      tbl.increments("id").index().unique();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+      tbl.string("author").notNullable();
+      tbl.string("title", 255).notNullable();
+      tbl
+        .string("topic", 255)
+        .references("id")
+        .inTable("topics")
+        .onUpdate("CASCADE");
+      tbl.string("content").notNullable();
+      tbl
+        .string("user_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
     })
-  
+
+    .createTable("comments", (tbl) => {
+      tbl.increments("id").index();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+      tbl.string("author").notNullable();
+      tbl.string("comment").notNullable();
+      tbl
+        .string("post_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("posts")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+
+    .createTable("notifications", (tbl) => {
+      tbl.increments();
+      tbl.string("reaction").notNullable();
+      tbl
+        .string("comment_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("comments")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema
-     .dropTableIfExists('users')
-     .dropTableIfExists('posts')
-     .dropTableIfExists('comments')
-     .dropTableIfExists('notifications')
-
+exports.down = function (knex, Promise) {
+  return knex.schema
+    .dropTableIfExists("users")
+    .dropTableIfExists("posts")
+    .dropTableIfExists("comments")
+    .dropTableIfExists("notifications");
 };
-
--- user/user-model.js
+++ user/user-model.js
@@ -1,50 +1,77 @@
 module.exports = {
-    addUser,
-    findUserById,
-    findUserByUsername, 
-    findUsers,
-    removeUser,
-    getUsers,
-}
+  addUser,
+  updateUser,
+  findUserById,
+  findUserByUsername,
+  findUsers,
+  removeUser,
+  getUsers,
+  getUserPost,
+  getUserDetails,
+};
 
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
+const postModel = require("../models/post-model.js");
 
 // GET STRAINS
 function getUsers() {
-    return db('users');
+  return db("users");
 }
 
-
 function addUser(newUser) {
-    return db('users')
-        .insert(newUser)
-        .then(ids => {
-            return findUserById(ids[0]);
-        });
+  return db("users")
+    .insert(newUser)
+    .then((ids) => {
+      return findUserById(ids[0]);
+    });
 }
 
 function findUserById(user_id) {
-    return db('users')
-        .where({ id: user_id })
-        .first();
+  return db("users").where({ id: user_id }).first();
 }
 
 function findUserByUsername(submitted_username) {
-    return db('users')
-        .where({ username: submitted_username})
-        .first();
+  return db("users").where({ username: submitted_username }).first();
 }
 
 function findUsers() {
-    return db('users')
-    .select('users.id', 'users.username');
+  return db("users").select("users.id", "users.username");
+}
+
+function getUser(user_id) {
+  return db("users").where({ id: user_id }).first();
+}
+
+function updateUser(user_id, updatedUser) {
+  return db("users")
+    .where({ id: user_id })
+    .update(updatedUser)
+    .then((count) => {
+      return getUser(user_id);
+    });
 }
 
 function removeUser(user_id) {
-    return db('users')
-        .where({ id: user_id })
-        .del();
+  return db("users").where({ id: user_id }).del();
 }
 
+function getUserPost(user_id) {
+  return db("posts")
+    .join("users", "users.user_id", "posts.user_id")
+    .select("posts.post_id", "posts.name")
+    .where("posts.user_id", user_id);
+}
 
+async function getUserDetails(user_id) {
+  const user = await getUser(user_id);
+  const post = await postModel.getUserPost(user_id);
 
+  return {
+    id: user.id,
+    username: user.username,
+    first_name: user.first_name,
+    last_name: user.last_name,
+    email: user.email,
+    posts: post,
+  };
+}

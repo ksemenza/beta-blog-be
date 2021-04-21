a6dcfb343ac9cff0a85de16fd3c31fb7d6bf256d
ksemenza <ksemenza13@gmail.com>
2021-04-21 18:01:53 -0400

    update models

--- models/comment-model.js
+++ models/comment-model.js
@@ -1,70 +1,61 @@
 module.exports = {
-    getCommentsAll,
-    addComment,
-    getComment,
-    getPostComment,
-    removeComment,
-    updateComment,
-    getCommentDetails
-}
+  getCommentsAll,
+  addComment,
+  getComment,
+  getPostComment,
+  removeComment,
+  updateComment,
+  getCommentDetails,
+};
 
-const db = require('../data/db-config.js')
-const notificationModel = require('./notification-model.js')
-const postModel = require('./post-model.js')
+const db = require("../data/db-config.js");
+const notificationModel = require("./notification-model.js");
 
-
-function getCommentsAll(){
-    return db('comments')
+function getCommentsAll() {
+  return db("comments");
 }
 
 function addComment(newComment) {
-    return db('comments')
-        .insert(newComment)
-        .then(ids => {
-            return getComment(ids[0]);
-        });
+  return db("comments")
+    .insert(newComment)
+    .then((ids) => {
+      return getComment(ids[0]);
+    });
 }
 
 function getComment(post_id) {
-    return db('posts')
-    .where({id: post_id})
-    .first();
+  return db("posts").where({ id: post_id }).first();
 }
 
 function getPostComment(post_id) {
-    return db('comments')
-    .where({'post_id' : post_id}) 
+  return db("comments").where({ post_id: post_id });
 }
 
 function removeComment(comment_id) {
-    return db('comments')
-        .where({ id: comment_id })
-        .del();
+  return db("comments").where({ id: comment_id }).del();
 }
 
 function updateComment(comment_id, updatedComment) {
-    return db('comments')
-    .where({id: comment_id})
+  return db("comments")
+    .where({ id: comment_id })
     .update(updatedComment)
-    .then(count => {
-        return getComment(comment_id)
-    })
+    .then((count) => {
+      return getComment(comment_id);
+    });
 }
 
 async function getCommentDetails(comment_id) {
-    const comment = await getComment(comment_id)
-    const notification = await notificationModel.getCommentNotification(comment_id)
-    const post = await postModel.getPostDetails(comment_id)
+  const comment = await getComment(comment_id);
+  const notification = await notificationModel.getCommentNotification(
+    comment_id
+  );
 
-    return {
-        'user_id':comment.user_id,
-        'post_id': comment.post_id,
-        'author':comment.author,
-        'id': comment.id,
-        'comment':comment.comment,
-        'notification': notification,
-        'posts':post
-    }
+  return {
+    user_id: comment.user_id,
+    post_id: comment.post_id,
+    author: comment.author,
+    id: comment.id,
+    comment: comment.comment,
+    notification: notification,
+  };
 }
-
-
--- models/post-model.js
+++ models/post-model.js
@@ -1,87 +1,75 @@
 module.exports = {
-    getPostsAll,
-    getPosts,
-    addPost,
-    getPost,
-    getUserPost,
-    removePost,
-    updatePost,
-    getPostDetails
-}
+  getPostsAll,
+  getPosts,
+  addPost,
+  getPost,
+  getUserPost,
+  removePost,
+  updatePost,
+  getPostDetails,
+};
 
-const commentModel = require('./comment-model.js')
-const userModel = require('../user/user-model.js')
-const db = require('../data/db-config.js')
+const commentModel = require("./comment-model.js");
+const db = require("../data/db-config.js");
 
-function getPostsAll(){
-    return db('posts')
+function getPostsAll() {
+  return db("posts");
 }
 
 function addPost(newPost) {
-    return db('posts')
-        .insert(newPost)
-        .then(ids => {
-            return getPost(ids[0]);
-        });
+  return db("posts")
+    .insert(newPost)
+    .then((ids) => {
+      return getPost(ids[0]);
+    });
 }
 
 function getPost(post_id) {
-    return db('posts')
-    .where({id: post_id})
-    .first();
+  return db("posts").where({ id: post_id }).first();
 }
 
 function getPosts(post_id) {
-    return db('posts')
-    .join('users', 'users.id', 'post.user_id')
+  return db("posts")
+    .join("users", "users.id", "post.user_id")
     .select(
-        'post.id as post_id',
-        'post.title as title',
-        'post.content as content',
-        'post.topic as topic',
-        'users.username as username'
+      "post.id as post_id",
+      "post.title as title",
+      "post.content as content",
+      "post.topic as topic",
+      "users.username as username"
     )
-    .where({'post_id':post_id})
+    .where({ post_id: post_id })
     .first();
 }
 
 function getUserPost(user_id) {
-    return db('posts')
-    .where({'user_id' : user_id}) 
+  return db("posts").where({ user_id: user_id });
 }
 
 function removePost(post_id) {
-    return db('posts')
-        .where({ id: post_id })
-        .del();
+  return db("posts").where({ id: post_id }).del();
 }
 
 function updatePost(post_id, updatedPost) {
-    return db('posts')
-    .where({id: post_id})
+  return db("posts")
+    .where({ id: post_id })
     .update(updatedPost)
-    .then(count => {
-        return getPost(post_id)
-    })
+    .then((count) => {
+      return getPost(post_id);
+    });
 }
 
-
-//
 async function getPostDetails(post_id) {
-    const post = await getPost(post_id)
-    const comment = await commentModel.getPostComment(post_id)
-    const user = await userModel.findUserById(post_id)
+  const post = await getPost(post_id);
+  const comment = await commentModel.getPostComment(post_id);
 
-    return {
-        'user_id': post.user_id,
-        'author':post.author,
-        'id': post.id,
-        'title':post.title,
-        'content':post.content,
-        'topic':post.topic,
-        'comments': comment,
-        'users':user
-    }
+  return {
+    user_id: post.user_id,
+    author: post.author,
+    id: post.id,
+    title: post.title,
+    content: post.content,
+    topic: post.topic,
+    comments: comment,
+  };
 }
-
-

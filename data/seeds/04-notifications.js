1a3021f114aa8e0853d2935c4cc29be5b7c530c2
Kim Semenza <ksemenza13@gmail.com>
2022-09-21 07:59:33 -0400

    Update 04-notifications.js

--- data/seeds/04-notifications.js
+++ data/seeds/04-notifications.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('notifications').del()
+  return knex('notifications')
     .then(function () {
       // Inserts seed entries
       return knex('notifications').insert([
